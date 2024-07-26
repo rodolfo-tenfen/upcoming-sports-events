47dc3657d2e041f605d8e6105f41132c3b24e06a
Rodolfo Tenfen <rodolfo.tenfen@gmail.com>
2024-07-26 14:58:12 -0300

    Configure ktlint

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,13 @@
-// Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
     alias(libs.plugins.android.application) apply false
     alias(libs.plugins.jetbrains.kotlin.android) apply false
     alias(libs.plugins.ktlint.gradle)
+}
+
+ktlint {
+    filter {
+        exclude { element ->
+            element.file.path.contains("generated")
+        }
+    }
 }
