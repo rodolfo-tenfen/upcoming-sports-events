943ca7bf8003d71d7b9dc8aac1292640a8741b66
Rodolfo Tenfen <rodolfo.tenfen@gmail.com>
2024-07-28 16:45:16 -0300

    Create :presentation:theme module

--- presentation/theme/src/main/java/tenfen/rodolfo/presentation/theme/Type.kt
+++ presentation/theme/src/main/java/tenfen/rodolfo/presentation/theme/Type.kt
@@ -1,4 +1,4 @@
-package tenfen.rodolfo.upcomingsportsevents.ui.theme
+package tenfen.rodolfo.presentation.theme
 
 import androidx.compose.material3.Typography
 import androidx.compose.ui.text.TextStyle
