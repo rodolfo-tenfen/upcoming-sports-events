668a096ab4e10ce385a9956e3a0d7c2b82828f52
Rodolfo Tenfen <rodolfo.tenfen@gmail.com>
2024-07-29 13:38:45 -0300

    Implement :presentation:theme module

--- presentation/theme/src/main/java/tenfen/rodolfo/presentation/theme/Color.kt
+++ presentation/theme/src/main/java/tenfen/rodolfo/presentation/theme/Color.kt
@@ -2,10 +2,10 @@ package tenfen.rodolfo.presentation.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple80 = Color(0xFFD0BCFF)
-val PurpleGrey80 = Color(0xFFCCC2DC)
-val Pink80 = Color(0xFFEFB8C8)
-
-val Purple40 = Color(0xFF6650a4)
-val PurpleGrey40 = Color(0xFF625b71)
-val Pink40 = Color(0xFF7D5260)
+val cantaloupe = Color(0xFFFFAB30)
+val deepOrange = Color(0xFFE7410F)
+val azure = Color(0xFF0094FF)
+val charcoal = Color(0xFF343434)
+val mountainMist = Color(0xFF989696)
+val dustyGray = Color(0xFFA29898)
+val carbonGray = Color(0xFF635D5D)
